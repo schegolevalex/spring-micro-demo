fa5db3d09c012208554954a92646045b24e3ab37
Alexey Schegolev <schegolevalex@gmail.com>
2023-02-18 18:44:20 +0300

    create new client-2 module

--- eureka-client/src/main/java/com/schegolevalex/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/schegolevalex/micro/demo/eurekaclient/controller/TestController.java
@@ -1,14 +1,15 @@
 package com.schegolevalex.micro.demo.eurekaclient.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/main")
 public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return "asdsad1 sadasd1 1262112";
     }
 }
