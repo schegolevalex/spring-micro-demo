dc4c56905bf5adb1afd16a2c5f60c95e00a50563
Alexey Schegolev <schegolevalex@gmail.com>
2023-03-14 12:35:13 +0300

    wip

--- eureka-client/src/main/java/com/schegolevalex/micro/demo/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/schegolevalex/micro/demo/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.schegolevalex.micro.demo.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
+    @Value("${tmpVar}")
+    private String tmpVar;
+
     @GetMapping("/test")
     public String test() {
-        return "asdsad1 sadasd1 1262112";
+        return "id = " + id + "; " + "tmpVar = " + tmpVar;
     }
+
 }
